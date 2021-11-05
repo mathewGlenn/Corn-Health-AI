485be65df86286f23348fd8b95c09a12a2f23059
mathewGlenn <81506680+mathewGlenn@users.noreply.github.com>
2021-11-05 13:32:56 +0800

    add softmax function

--- app/src/main/java/com/glennappdev/cornhealthai/Classifier.kt
+++ app/src/main/java/com/glennappdev/cornhealthai/Classifier.kt
@@ -5,12 +5,15 @@ import org.pytorch.IValue
 import org.pytorch.Module
 import org.pytorch.Tensor
 import org.pytorch.torchvision.TensorImageUtils
+import kotlin.math.pow
 
 class Classifier(modelPath: String?) {
 
     var model: Module = Module.load(modelPath)
     var mean = floatArrayOf(0.485f, 0.456f, 0.406f)
     var std = floatArrayOf(0.229f, 0.224f, 0.225f)
+
+    private val e = 2.71828f
 
     fun setMeanAndStd(mean: FloatArray, std: FloatArray) {
         this.mean = mean
@@ -23,34 +26,57 @@ class Classifier(modelPath: String?) {
         return TensorImageUtils.bitmapToFloat32Tensor(bitmap, mean, std)
     }
 
-    fun argMax(inputs: FloatArray): Int {
-        var maxIndex = -1
-        var maxvalue = 0.0f
+    fun softMax(inputs: FloatArray): MutableList<Float> {
+        val softMaxProbs: MutableList<Float> = ArrayList()
+        var sumOfProbs = 0.0f
         for (i in inputs.indices) {
-            if (inputs[i] > maxvalue) {
-                maxIndex = i
-                maxvalue = inputs[i]
-            }
+            sumOfProbs += (e.pow(inputs[i]))
         }
-        return maxIndex
+        for (i in inputs.indices) {
+            softMaxProbs.add(e.pow(inputs[i]) / sumOfProbs)
+        }
+        return softMaxProbs
     }
 
+    data class Max(var argMax: Int, var maxProbability: Float)
 
-    fun predict(bitmap: Bitmap?, flag: String?): String {
+    fun argMax(inputs: MutableList<Float>): Max {
+        var maxIndex = -1
+        var maxValue = 0.0f
+        for (i in inputs.indices) {
+            if (inputs[i] > maxValue) {
+                maxIndex = i
+                maxValue = inputs[i]
+            }
+        }
+        return Max(maxIndex, maxValue)
+    }
+
+    data class Prediction(
+        var predClass: String,
+        var predSciName: String,
+        var predProbability: String,
+    )
+
+    fun predict(bitmap: Bitmap?, model: String?): Prediction {
         val tensor = preprocess(bitmap, 224)
         val inputs = IValue.from(tensor)
-        val outputs = model.forward(inputs).toTensor()
+        val outputs = this.model.forward(inputs).toTensor()
         val scores = outputs.dataAsFloatArray
-        val classIndex = argMax(scores)
+        val (classIndex, maxValue) = argMax(softMax(scores))
 
-        lateinit var predictedClass: String
+        var predictedClass = ""
+        var classSciName = ""
+        val predictionProbability = "%.2f".format(maxValue * 100) + "%"
 
-        if (flag == "insect_pest") {
+        if (model == "insect_pest") {
             predictedClass = Constants.INSECT_PEST[classIndex]
-        } else if (flag == "leaf_disease") {
+            classSciName = Constants.INSECT_PEST_SCIENTIFIC_NAMES[classIndex]
+        } else if (model == "leaf_disease") {
             predictedClass = Constants.LEAF_DISEASES[classIndex]
+            classSciName = Constants.LEAF_DISEASES_SCIENTIFIC_NAMES[classIndex]
         }
-        return predictedClass
+        return Prediction(predictedClass, classSciName, predictionProbability)
     }
 
 }
