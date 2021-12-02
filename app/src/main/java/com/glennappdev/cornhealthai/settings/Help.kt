7297de2088ee05ad6e2f13344c7e3054bc893a2c
mathewGlenn <81506680+mathewGlenn@users.noreply.github.com>
2021-12-02 20:51:57 +0800

    - Help Activity

---
+++ app/src/main/java/com/glennappdev/cornhealthai/settings/Help.kt
@@ -0,0 +1,32 @@
+package com.glennappdev.cornhealthai.settings
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.glennappdev.cornhealthai.R
+import com.glennappdev.cornhealthai.databinding.ActivityHelpBinding
+
+class Help : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityHelpBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+
+        // checked saved locale
+        val preferences: SharedPreferences =
+            this.getSharedPreferences("LANGUAGE", Context.MODE_PRIVATE)
+        val language = preferences.getString("SAVED_LANGUAGE", "en")
+        val localeHelper = LocaleHelper()
+        val context = localeHelper.setLocale(this, language.toString())
+        val resources = context.resources
+
+        binding.step1.text = resources.getString(R.string.step1)
+        binding.step2.text = resources.getString(R.string.step2)
+        binding.step3.text = resources.getString(R.string.step3)
+        binding.step4.text = resources.getString(R.string.step4)
+    }
+}
